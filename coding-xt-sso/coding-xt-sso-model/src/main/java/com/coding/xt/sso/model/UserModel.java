33126d718cae45aa9a27e79c8d4a4aeed1b424f1
yxym0203 <1500081802@qq.com>
2022-09-18 15:06:17 +0800

    登录用户的信息

--- coding-xt-sso/coding-xt-sso-model/src/main/java/com/coding/xt/sso/model/UserModel.java
+++ coding-xt-sso/coding-xt-sso-model/src/main/java/com/coding/xt/sso/model/UserModel.java
@@ -5,5 +5,32 @@ package com.coding.xt.sso.model;
  * @create 2022-09-17 下午 10:44
  */
 
+import lombok.Data;
+
+@Data
 public class UserModel {
+
+    private String nickname;
+
+    private Integer sex;
+
+    private String city;
+
+    private String province;
+
+    private String country;
+
+    private String headImageUrl;
+
+    private String mobile;
+
+    private String name;
+
+    private String inviteUrl;
+
+    private String school;
+
+    private String area;
+
+    private String grade;
 }
