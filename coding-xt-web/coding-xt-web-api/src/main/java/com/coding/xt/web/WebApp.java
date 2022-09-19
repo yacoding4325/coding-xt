dcce5b51c2fcc14ba64b3d6a2961be62d80f1d6f
yxym0203 <1500081802@qq.com>
2022-09-19 21:18:55 +0800

    创建web模块

---
+++ coding-xt-web/coding-xt-web-api/src/main/java/com/coding/xt/web/WebApp.java
@@ -0,0 +1,17 @@
+package com.coding.xt.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-19 下午 9:05
+ */
+@SpringBootApplication
+public class WebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class,args);
+    }
+
+}
