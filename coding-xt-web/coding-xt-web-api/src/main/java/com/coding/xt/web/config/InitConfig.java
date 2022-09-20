3bf26164c257434ef432f2dfc413fe1cb5ed66c1
yxym0203 <1500081802@qq.com>
2022-09-20 10:34:44 +0800

    扫包路劲，和分页插件及登录接口api

---
+++ coding-xt-web/coding-xt-web-api/src/main/java/com/coding/xt/web/config/InitConfig.java
@@ -0,0 +1,13 @@
+package com.coding.xt.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-19 下午 9:07
+ */
+@Configuration
+@ComponentScan({"com.coding.xt.common.service"})
+public class InitConfig {
+}
---
+++ coding-xt-web/coding-xt-web-api/src/main/java/com/coding/xt/web/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.coding.xt.web.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-19 下午 9:08
+ */
+
+@Configuration
+@MapperScan("com.coding.xt.web.dao")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+}
