7181512a5e020ad5bc1186859ec552ba2d40d7c6
yxym0203 <1500081802@qq.com>
2022-09-20 10:45:19 +0800

    mybatis分页查询接口

---
+++ coding-xt-web/coding-xt-web-dao/src/main/java/com/coding/xt/web/dao/NewsMapper.java
@@ -0,0 +1,13 @@
+package com.coding.xt.web.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.coding.xt.pojo.News;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-19 下午 9:54
+ */
+
+public interface NewsMapper extends BaseMapper<News> {
+}
