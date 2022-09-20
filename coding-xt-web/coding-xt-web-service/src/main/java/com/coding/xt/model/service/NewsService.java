e3cc05173c16b63cb43889dea1e8895f90de99c4
yxym0203 <1500081802@qq.com>
2022-09-20 10:50:03 +0800

    新闻服务接口

---
+++ coding-xt-web/coding-xt-web-service/src/main/java/com/coding/xt/model/service/NewsService.java
@@ -0,0 +1,21 @@
+package com.coding.xt.model.service;
+
+
+import com.coding.xt.common.model.CallResult;
+import com.coding.xt.web.model.params.NewsParam;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-19 下午 9:20
+ */
+
+
+public interface NewsService {
+
+    CallResult newsList(NewsParam newsParam);
+
+    CallResult newsDetailList(NewsParam newsParam);
+
+    CallResult findNewsById(NewsParam newsParam);
+
+}
