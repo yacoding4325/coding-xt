fc908f71cf08027e446cfe93b69df99c3a19ad61
yxym0203 <1500081802@qq.com>
2022-09-20 10:48:43 +0800

    新闻模型，参数及code枚举--解析产出的参数是否合法(添加)

---
+++ coding-xt-web/coding-xt-web-model/src/main/java/com/coding/xt/web/model/enums/Status.java
@@ -0,0 +1,58 @@
+package com.coding.xt.web.model.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-19 下午 9:57
+ */
+
+public enum Status {
+    /**
+     * look name
+     */
+    NORMAL(0,"正常"),
+    DELETE(1,"删除");
+
+    private static final Map<Integer, Status> CODE_MAP = new HashMap<>(3);
+
+    static{
+        for(Status status: values()){
+            CODE_MAP.put(status.getCode(), status);
+        }
+    }
+
+    /**
+     * 根据code获取枚举值
+     * @param code
+     * @return
+     */
+    public static Status valueOfCode(int code){
+        return CODE_MAP.get(code);
+    }
+
+    private int code;
+    private String msg;
+
+    Status(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
