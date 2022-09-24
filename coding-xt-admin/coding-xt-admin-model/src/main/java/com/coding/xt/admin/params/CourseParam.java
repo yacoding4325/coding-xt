eb44fa8e8425a595c0bfb713184fd2ab2d6e0f61
yxym0203 <1500081802@qq.com>
2022-09-24 15:57:02 +0800

    ...

---
+++ coding-xt-admin/coding-xt-admin-model/src/main/java/com/coding/xt/admin/params/CourseParam.java
@@ -0,0 +1,35 @@
+package com.coding.xt.admin.params;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-23 下午 10:16
+ */
+//课程参数
+@Data
+public class CourseParam {
+
+    private Long userId;
+    private Long id;
+    private String courseName;
+    private String courseDesc;
+    private String subjects;
+    private BigDecimal coursePrice;
+    private BigDecimal courseZhePrice;
+    private Integer courseStatus;
+    private List<Long> subjectIdList;
+    private Integer orderTime;//订购时长
+    private int page = 1;
+    private int pageSize = 20;
+    private String subjectName;
+    private String subjectGrade;
+    private String subjectTerm;
+
+    private Long courseId;
+    private String imageUrl;
+
+}
