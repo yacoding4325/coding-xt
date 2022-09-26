19cae55d639fec47d116f838a4ffe922fbe2618a
yxym0203 <1500081802@qq.com>
2022-09-26 20:17:55 +0800

    CourseParam参数

--- coding-xt-web/coding-xt-web-model/src/main/java/com/coding/xt/web/model/params/UserCourseParam.java
+++ coding-xt-web/coding-xt-web-model/src/main/java/com/coding/xt/web/model/params/UserCourseParam.java
@@ -2,6 +2,9 @@ package com.coding.xt.web.model.params;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * @Author yaCoding
  * @create 2022-09-25 下午 4:01
@@ -10,15 +13,35 @@ import lombok.Data;
 @Data
 public class UserCourseParam {
 
-    public Long getCourseId() {
-        return null;
-    }
+//    public Long getCourseId() {
+//        return null;
+//    }
+//
+//    public Long getUserId() {
+//        return null;
+//    }
+//
+//    public Long getTime() {
+//        return null;
+//    }
 
-    public Long getUserId() {
-        return null;
-    }
+    private Long time;
+    private Long userId;
+    private Long id;
+    private String courseName;
+    private String courseDesc;
+    private String subjects;
+    private BigDecimal coursePrice;
+    private BigDecimal courseZhePrice;
+    private Integer courseStatus;
+    private List<Integer> subjectIdList;
+    private Integer orderTime;//订购时长
+    private int page = 1;
+    private int pageSize = 20;
+    private String subjectName;
+    private String subjectGrade;
+    private String subjectTerm;
 
-    public Long getTime() {
-        return null;
-    }
+    private Long courseId;
+    private String imageUrl;
 }
